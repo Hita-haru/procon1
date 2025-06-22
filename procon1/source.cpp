f16145041ba5c9633a3385c93def4b9c97a6ed9a
Hira_haru <143330363+Hita-haru@users.noreply.github.com>
2025-06-22 21:20:11 +0900

    v0.0.2

--- procon1/source.cpp
+++ procon1/source.cpp
@@ -12,10 +12,15 @@
 #include <iostream>
 #include <curses.h>
 
-//使用中変数一覧
-//inputch1 => main関数内、switch文入力用 char型
+// 使用中変数一覧
+// startwin => main関数内、スタートウィンドウ WINDOW*型
+// inputch1 => main関数内、switch文入力用 char型
+
+//プロトタイプ宣言
+int msgbox(int y, int x, const char* msg);
 
 int main() {
+	char inpch1;
 	initscr();
 	cbreak();
 	echo();
@@ -23,16 +28,17 @@ int main() {
 	init_pair(1, COLOR_WHITE, COLOR_BLUE);
 
 	//startwinの作成
-	WINDOW* startwin = newwin(10, 50, 2, 2);
+	WINDOW* startwin = newwin(20, 60, 2, 2);
 	mvwprintw(startwin,2,4,"Procon1"); //"Procon1"は後ほど変更
 	mvwprintw(startwin, 4, 4, "hキーを押してヘルプを表示");
 	bkgd(COLOR_PAIR(1));
 	box(startwin, '|', '=');
 	refresh();
 	
-	char inpch1 = wgetch(startwin);
+	inpch1 = wgetch(startwin);
 
 	while (1) {
+		clear();
 		switch (inpch1) {
 		case 'h':
 		case 'H':
@@ -40,9 +46,14 @@ int main() {
 			break;
 		case 'x':
 		case 'X':
-			endwin();
-			return 0;
-			break;
+			inpch1 = msgbox(4, 10, "終了しまか？ (y/n)");
+			if (inpch1 == 'y' || inpch1 == 'Y') {
+				delwin(startwin);
+				endwin();
+				return 0;
+			} else {
+				msgbox(6, 12, "キャンセルされました。");
+			}
 		default:
 			mvwprintw(startwin, 6, 2, "無効なキーです。もう一度入力してください。");
 			wrefresh(startwin);
@@ -51,4 +62,14 @@ int main() {
 		}
 	}
 	return 0;
+}
+
+int msgbox(int y, int x, const char* msg) {
+	WINDOW* msgwin = newwin(10, 40, y, x);
+	box(msgwin, '|', '=');
+	mvwprintw(msgwin, 2, 2, "%s", msg);
+	wrefresh(msgwin);
+	int ch = wgetch(msgwin);
+	delwin(msgwin);
+	return ch;
 }
