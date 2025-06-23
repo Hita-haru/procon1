e5abc65767f7f37a6f4f0dda2369d5cc3c7d1818
Hira_haru <143330363+Hita-haru@users.noreply.github.com>
2025-06-23 23:46:24 +0900

    v0.0.4

--- procon1/source.cpp
+++ procon1/source.cpp
@@ -15,6 +15,7 @@
 
 #include <stdio.h>
 #include <iostream>
+#include <stdlib.h>
 #include <curses.h>
 
 // 使用中変数一覧
@@ -24,20 +25,36 @@
 // msg => msgbox関数内、メッセージ内容 const char*型
 // ch => msgbox関数内、メッセージボックス入力用 int型
 // y, x => msgbox関数内、メッセージボックスの位置 int型
-
+// helpwin => main関数内、ヘルプウィンドウ WINDOW*型
+// inpstr1 => chkwnd関数内、認証ウィンドウ入力 char[]型
+// inpch2 => chkwnd関数内、認証ウィンドウ入力用 char型
+// chkwnd1 => chkwnd関数内、認証ウィンドウ WINDOW*型
+// stat1 => chkwnd関数内、認証ステータス int型
+// level => chkwnd関数内、認証レベル int型
 
 //プロトタイプ宣言
 int msgbox(int y, int x, const char* msg);
+int errorbox(int y, int x, const char* msg);
+int successbox(int y, int x, const char* msg);
+int chkwnd(int level);
+int errorex(int errorcode);
+
+//グローバル変数
+int max_x,max_y,min_x,min_y;
 WINDOW* helpwin;
 
 int main() {
-	
 	char inpch1;
 	initscr();
+	getmaxyx(stdscr, max_y, max_x);
+	min_x = max_x - 4;
+	min_y = max_y - 4;
 	cbreak();
 	noecho();
 	start_color();
 	init_pair(1, COLOR_WHITE, COLOR_BLUE);
+	init_pair(2, COLOR_WHITE, COLOR_RED);
+	init_pair(3, COLOR_WHITE, COLOR_GREEN);
 
 	//startwinの作成
 	WINDOW* startwin = newwin(20, 60, 2, 2);
@@ -51,6 +68,10 @@ int main() {
 		refresh();
 		inpch1 = wgetch(startwin);
 		switch (inpch1) {
+		case '#':
+			if (chkwnd(2) == 1) {
+				errorex(2);
+			}
 		case 'h':
 		case 'H':
 			helpwin = newwin(20, 60, 4, 4);
@@ -71,10 +92,11 @@ int main() {
 				endwin();
 				return 0;
 			} else {
-				msgbox(6, 12, "キャンセルされました。");
+				errorbox(6, 12, "キャンセルされました。");
 			}
+			break;
 		default:
-			msgbox(6, 10, "無効な入力です。Hキーでヘルプを表示");
+			errorbox(6, 10, "無効な入力です。Hキーでヘルプを表示");
 			break;
 		}
 	}
@@ -89,4 +111,105 @@ int msgbox(int y, int x, const char* msg) {
 	int ch = wgetch(msgwin);
 	delwin(msgwin);
 	return ch;
+}
+
+int errorbox(int y, int x, const char* msg) {
+	WINDOW* msgwin = newwin(10, 40, y, x);
+	box(msgwin, '|', '=');
+	wbkgd(msgwin, COLOR_PAIR(2));
+	mvwprintw(msgwin, 2, 2, "%s", msg);
+	wrefresh(msgwin);
+	int ch = wgetch(msgwin);
+	delwin(msgwin);
+	return ch;
+}
+
+int successbox(int y, int x, const char* msg) {
+	WINDOW* msgwin = newwin(10, 40, y, x);
+	box(msgwin, '|', '=');
+	wbkgd(msgwin, COLOR_PAIR(3));
+	mvwprintw(msgwin, 2, 2, "%s", msg);
+	wrefresh(msgwin);
+	int ch = wgetch(msgwin);
+	delwin(msgwin);
+	return ch;
+}
+
+int chkwnd(int level) {
+	char inpstr1[20] = "NULL";
+	char inpch2;
+	int stat1;
+	WINDOW* chkwnd1 = newwin(10, 30, 5, 5);
+	box(chkwnd1, '|', '=');
+	mvwprintw(chkwnd1, 2, 2, "認証ウィンドウ");
+	switch (level) {
+	case 1:
+		mvwprintw(chkwnd1, 4, 2, "続けますか？(y/n)");
+		break;
+	case 2:
+		wbkgd(chkwnd1, COLOR_PAIR(2));
+		mvwprintw(chkwnd1, 4, 2, "管理者用パスワードを入力");
+		break;
+	case 3:
+		wbkgd(chkwnd1, COLOR_PAIR(3));
+		mvwprintw(chkwnd1, 4, 2, "危険な操作です。続けますか？(y/n)");
+		break;
+	default:
+		errorbox(6, 10, "無効な値での呼び出し");
+		endwin();
+		return -1;
+		break;
+	}
+	mvwprintw(chkwnd1, 6, 2, "レベル: %d", level);
+	wmove(chkwnd1, 8, 2);
+	if (level == 2) {
+		wprintw(chkwnd1, "パスワード: ");
+		echo();
+		wgetstr(chkwnd1, inpstr1); // パスワード入力
+		noecho();
+		if (atoi(inpstr1) == 79417432) {
+			stat1 = 1;
+		}
+		else {
+			stat1 = 0;
+		}
+	} else {
+		wprintw(chkwnd1, "入力: ");
+		inpch2 = wgetch(chkwnd1); // ユーザー入力
+		if (inpch2 == 'y' || inpch2 == 'Y') {
+			stat1 = 1;
+		}
+		else {
+			stat1 = 0;
+		}
+	}
+	if (stat1 == 1) {
+		successbox(8, 8, "認証に成功しました");
+		return 1;
+	}
+	else {
+		errorbox(8, 8, "認証に失敗しました");
+		return 0;
+	}
+}
+
+int errorex(int errorcode) {
+	WINDOW* sod = newwin(min_y, min_x, 2, 2);
+	box(sod, '|', '=');
+	mvwprintw(sod, 2, 2, ":(");
+	mvwprintw(sod, 4, 2, "エラーが発生しました。ソフトを終了します。");
+	mvwprintw(sod, 10, 2, "何かキーを押して終了...");
+	switch (errorcode) {
+	case 1:
+		mvwprintw(sod, 6, 2, "不明なエラー");
+		mvwprintw(sod, 8, 2, "Unknown error");
+		wbkgd(sod, COLOR_PAIR(2));
+	default:
+		mvwprintw(sod, 6, 2, "エラー情報を取得できませんでした");
+		mvwprintw(sod, 8, 2, "Undefined error");
+		wbkgd(sod, COLOR_PAIR(2));
+	}
+	wrefresh(sod);
+	wgetch(sod);
+	exit(-1);
 }
